06d65864181d625590a38c3e0e9c462467fb059e
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-04-29 16:43:19 +0200

    exo7 timer improved, ...

--- exo7Middleware/server.js
+++ exo7Middleware/server.js
@@ -1,48 +1,39 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  try {
-    // ICI MIDDLEWARE A EXECUTER AVANT DE TRAITER LA REQUETE.
     let start_time = new Date();
 
-    console.log(req.httpVersion, req.url, req.method);
-    // FIN MIDDLEWARE
-    if (req.url === "/") 
-    {
-        res.writeHead(200, {'content-type':'text/html'});
-        res.write('<h1>HOMEPAGE</h1>');
-
-        let finish_time = new Date();
-        let difference = new Date();
-        difference.setTime(finish_time.getTime() - start_time.getTime());
-        console.log(difference.getMilliseconds());
-
-        res.end();
+    try {
+        // ICI MIDDLEWARE A EXECUTER AVANT DE TRAITER LA REQUETE.
+        console.log(req.httpVersion, req.url, req.method);
+        // FIN MIDDLEWARE
+        if (req.url === "/") 
+        {
+            res.writeHead(200, {'content-type':'text/html'});
+            res.write('<h1>HOMEPAGE</h1>');
+        } 
+        else 
+        {
+            res.writeHead(404, {'content-type':'text/html'});
+            res.write('<h1>404 Not Found</h1>');
+        }
     } 
-    else 
+    catch (err) 
     {
-        res.writeHead(404, {'content-type':'text/html'});
-        res.write('<h1>404 Not Found</h1>');
-
-        let finish_time = new Date();
-        let difference = new Date();
-        difference.setTime(finish_time.getTime() - start_time.getTime());
-        console.log(difference.getMilliseconds());
-
-        res.end()
+        res.writeHead(500, {'content-type':'text/html'});
+        res.write('<h1>500 Internal Server Error</h1>');
     }
-  } catch (err) 
-  {
-      res.writeHead(500, {'content-type':'text/html'});
-      res.write('<h1>500 Internal Server Error</h1>');
+    
+    let finish_time = new Date();
+    let difference = new Date();
+    difference.setTime(finish_time.getTime() - start_time.getTime());
+    
+    console.log("\n");
+    console.log("execution time");
+    console.log(difference.getMilliseconds());
 
-      let finish_time = new Date();
-      let difference = new Date();
-      difference.setTime(finish_time.getTime() - start_time.getTime());
-      console.log(difference.getMilliseconds());
+    res.end();
 
-      res.end()
-  }
 } );
 
 server.listen(3000);
